4c437dce8646fb7e559cb2e7097db6a9a5710824
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-03-18 20:21:28 +0300

    refactored confirmation link

--- src/main/java/ru/nidecker/nbanews/service/RegistrationService.java
+++ src/main/java/ru/nidecker/nbanews/service/RegistrationService.java
@@ -39,7 +39,7 @@ public class RegistrationService {
                         Set.of(Role.USER))
         );
 
-        String link = "http://localhost:8080/api/v1/registration/confirm?token=" + token;
+        String link = "https://nbanews.herokuapp.com//api/v1/registration/confirm?token=" + token;
         emailSender.send(
                 request.getEmail(),
                 buildEmail(request.getNickname(), link));
